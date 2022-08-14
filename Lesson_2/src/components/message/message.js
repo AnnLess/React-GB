ad008eaefb0b11b2935bca1de2d80ba3ede89ff7
AnnLess <78751680+AnnLess@users.noreply.github.com>
2022-08-14 15:40:52 +0300

    Revert "Initial commit"

--- Lesson_2/src/components/message/message.js
+++
@@ -1,8 +0,0 @@
-export function Message({message}) {
-    return (
-        <div class=" message__wrapper">
-            <h3 class= "message-text">{message.message}</h3>
-            <p class= "message-author">{message.author}</p>
-        </div>
-    )
-};
